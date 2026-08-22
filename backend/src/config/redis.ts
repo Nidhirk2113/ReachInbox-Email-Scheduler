a4e3fcedb8ddad182b33b67dd36d43b554f96ff2
Nidhi Kulkarni <nidhirk2113@gmail.com>
2026-08-22 10:16:59 +0530

    Refactor Redis import and initialization

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,7 +1,7 @@
-import IORedis from "ioredis";
+import { Redis } from "ioredis";
 import { env } from "./env.js";
 
-export const redis = new IORedis({
+export const redis = new Redis({
   host: env.redis.host,
   port: env.redis.port,
   maxRetriesPerRequest: null,
